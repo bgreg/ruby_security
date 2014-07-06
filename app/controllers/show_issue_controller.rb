d8dc3a5bf2abfe8d9a7d2b98ce2f343e465462e0
gregmcguirk <greg@paynearme.com>
2014-07-05 21:47:31 -0700

    better scrolling, some refactor

--- app/controllers/show_issue_controller.rb
+++ app/controllers/show_issue_controller.rb
@@ -8,41 +8,25 @@ class ShowIssueController < UIViewController
     rmq(self.view).apply_style :root_view
 
     @scroll_view = rmq.append(UIScrollView, :scroll_view).get
-    setup_scroll_view
     append_labels
   end
 
-  def setup_scroll_view
-    scroll_view_frame_size =
-      CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)
-    content_size =
-      CGSizeMake(self.view.frame.size.width, 1000)
-
-    @scroll_view.frame = scroll_view_frame_size
-    @scroll_view.pagingEnabled = true
-    @scroll_view.backgroundColor = UIColor.whiteColor
-    @scroll_view.contentSize = content_size
-    @scroll_view.showsHorizontalScrollIndicator = false
-    @scroll_view.showsVerticalScrollIndicator = false
-  end
-
   def append_labels
-    rmq(@scroll_view).append(UILabel, :release_date).get
-    rmq(@scroll_view).append(UILabel, :last_revised).get
-    rmq(@scroll_view).append(UILabel, :source).get
+    labels = [ :release_date_label,
+               :release_date_body,
+               :last_revised,
+               :source,
+               :summary,
+               :cvss_severity,
+               :cvss_base_score,
+               :impact_subscore,
+               :explotability_subscore,
+               :cvss_metrics,
+               :access_vector,
+               :access_complexity,
+               :authentication,
+               :impact_type ]
 
-    rmq(@scroll_view).append(UILabel, :overview).get
-      rmq(@scroll_view).append(UILabel, :summary).get
-
-    rmq(@scroll_view).append(UILabel, :cvss_severity).get
-      rmq(@scroll_view).append(UILabel, :cvss_base_score).get
-      rmq(@scroll_view).append(UILabel, :impact_subscore).get
-      rmq(@scroll_view).append(UILabel, :explotability_subscore).get
-
-    rmq(@scroll_view).append(UILabel, :cvss_metrics).get
-      rmq(@scroll_view).append(UILabel, :access_vector).get
-      rmq(@scroll_view).append(UILabel, :access_complexity).get
-      rmq(@scroll_view).append(UILabel, :authentication).get
-      rmq(@scroll_view).append(UILabel, :impact_type).get
+    labels.each {|label| rmq(@scroll_view).append(UILabel, label) }
   end
 end
--- app/stylesheets/show_issue_controller_stylesheet.rb
+++ app/stylesheets/show_issue_controller_stylesheet.rb
@@ -12,20 +12,32 @@ class ShowIssueControllerStylesheet < ApplicationStylesheet
   end
 
   def scroll_view(st)
-    # puts st.inspect
-    # st.view.frame = CGRectMake(0, 0, self.frame.size.width, self.frame.size.height);
-    # st.view.pagingEnabled = true
-    # st.background_color = color.white
-    # st.view.contentSize = CGSizeMake(400, 500)
-    # st.view.showsHorizontalScrollIndicator = true
-    # st.view.showsVerticalScrollIndicator = false
+    st.scroll_enabled = true
+    st.content_inset =UIEdgeInsetsMake(2,2,2,2)
+    st.paging = true
+    st.background_color = color.white
+    st.view.frame =
+      CGRectMake(0, 0, st.super_width, st.super_height)
+    st.view.contentSize =
+      CGSizeMake(st.super_width, 1000)
+    st.view.showsHorizontalScrollIndicator = false
+    st.view.showsVerticalScrollIndicator = false
   end
 
-  def release_date(st)
+  def release_date_label(st)
     st.view.adjustsFontSizeToFitWidth = 0
     st.view.numberOfLines = 0
-    st.frame = {t: 0, w: 300, h: 30}
-    st.text = "Release date: #{Time.now}"
+    st.frame = {t: 0, w: st.super_width, h: 30}
+    st.text = "Release date:"
+    st.color = color.red
+    st.font = font.small
+  end
+
+  def release_date_body(st)
+    st.view.adjustsFontSizeToFitWidth = 0
+    st.view.numberOfLines = 0
+    st.frame = {t: 60, w: st.super_width, h: 30}
+    st.text = "#{Time.now}"
     st.color = color.black
     st.font = font.small
   end
@@ -33,7 +45,7 @@ class ShowIssueControllerStylesheet < ApplicationStylesheet
   def last_revised(st)
     st.view.adjustsFontSizeToFitWidth = 0
     st.view.numberOfLines = 0
-    st.frame = {bp: 10, w: 300, h: 30}
+    st.frame = {bp: 10, w: st.super_width, h: 30}
     st.text = "Last Revised: #{Time.now}"
     st.color = color.black
     st.font = font.small
@@ -42,27 +54,21 @@ class ShowIssueControllerStylesheet < ApplicationStylesheet
   def source(st)
     st.view.adjustsFontSizeToFitWidth = 0
     st.view.numberOfLines = 0
-    st.frame = {bp: 10, w: 300, h: 30}
+    st.frame = {bp: 10, w: st.super_width, h: 30}
     st.text = "Source: US-CERT/NIST"
     st.color = color.black
     st.font = font.small
   end
 
-  def overview(st)
-    st.view.adjustsFontSizeToFitWidth = 0
-    st.view.numberOfLines = 0
-    st.frame = {bp: 10, w: 300, h: 30}
-    st.text = "Overview"
-    st.color = color.black
-    st.font = font.small
-  end
-
   def summary(st)
-    st.view.adjustsFontSizeToFitWidth = 0
-    st.view.numberOfLines = 0
+    st.text = "Summary: The Dragonfly gem 0.7 before 0.8.6 and 0.9.x before 0.9.13 for Ruby, when used with Ruby on Rails, allows remote attackers to execute arbitrary code via a crafted request."\
+              "Summary: The Dragonfly gem 0.7 before 0.8.6 and 0.9.x before 0.9.13 for Ruby, when used with Ruby on Rails, allows remote attackers to execute arbitrary code via a crafted request."
+    st.adjusts_font_size = true
+    st.number_of_lines = 0
     st.view.minimumFontSize = 16
-    st.frame = {bp: 10, w: 300, h: 300}
-    st.text = "Summary: The Dragonfly gem 0.7 before 0.8.6 and 0.9.x before 0.9.13 for Ruby, when used with Ruby on Rails, allows remote attackers to execute arbitrary code via a crafted request."
+    # st.size_to_fit
+    # todo: figure out adjusting text to size of string
+    st.frame = {bp: 10, w: st.super_width , h: 300}
     st.color = color.black
     st.font = font.small
   end
@@ -70,92 +76,81 @@ class ShowIssueControllerStylesheet < ApplicationStylesheet
   def cvss_severity(st)
     st.view.adjustsFontSizeToFitWidth = 0
     st.view.numberOfLines = 0
-
-    st.frame = {bp: 10, w: 300, h: 30}
+    st.frame = {bp: 10, w: st.super_width, h: 30}
     st.text = "CVSS Severity"
-    st.color = color.red
+    st.color = color.black
     st.font = font.small
   end
 
   def cvss_base_score(st)
     st.view.adjustsFontSizeToFitWidth = 0
     st.view.numberOfLines = 0
-    st.frame = {bp: 10, w: 300, h: 30}
-
+    st.frame = {bp: 10, w: st.super_width, h: 30}
     st.text = "CVSS v2 Base Score: 7.2"
-    st.color = color.red
+    st.color = color.black
     st.font = font.small
   end
 
   def impact_subscore(st)
     st.view.adjustsFontSizeToFitWidth = 0
     st.view.numberOfLines = 0
-
-    st.frame = {bp: 10, w: 300, h: 30}
+    st.frame = {bp: 10, w: st.super_width, h: 30}
     st.text = "Impact subscore: 6.2"
-    st.color = color.red
+    st.color = color.black
     st.font = font.small
   end
 
   def explotability_subscore(st)
     st.view.adjustsFontSizeToFitWidth = 0
     st.view.numberOfLines = 0
-
-    st.frame = {bp: 10, w: 300, h: 30}
+    st.frame = {bp: 10, w: st.super_width, h: 30}
     st.text = "Exploitability Subscore: 10.0"
-    st.color = color.red
+    st.color = color.black
     st.font = font.small
   end
 
   def cvss_metrics(st)
     st.view.adjustsFontSizeToFitWidth = 0
     st.view.numberOfLines = 0
-    st.frame = {bp: 10, w: 300, h: 30}
-
+    st.frame = {bp: 10, w: st.super_width, h: 30}
     st.text = "CVSS Metrics"
-    st.color = color.red
+    st.color = color.black
     st.font = font.small
   end
 
   def access_vector(st)
     st.view.adjustsFontSizeToFitWidth = 0
     st.view.numberOfLines = 0
-    st.frame = {bp: 10, w: 300, h: 30}
-
+    st.frame = {bp: 10, w: st.super_width, h: 30}
     st.text = "Access Vector: Network Exploitable"
-    st.color = color.red
+    st.color = color.black
     st.font = font.small
   end
 
   def access_complexity(st)
     st.view.adjustsFontSizeToFitWidth = 0
     st.view.numberOfLines = 0
-    st.frame = {bp: 10, w: 300, h: 30}
-
+    st.frame = {bp: 10, w: st.super_width, h: 30}
     st.text = "Access Complexity: Low"
-    st.color = color.red
+    st.color = color.black
     st.font = font.small
   end
 
   def authentication(st)
     st.view.adjustsFontSizeToFitWidth = 0
     st.view.numberOfLines = 0
-
-    st.frame = {bp: 10, w: 300, h: 30}
-    st.text = "Authentication: Not required to exploit"
-    st.color = color.red
+    st.frame = {bp: 10, w: st.super_width, h: 30}
+    st.text = "Authentication: Not requiblack to exploit"
+    st.color = color.black
     st.font = font.small
   end
 
   def impact_type(st)
     st.view.adjustsFontSizeToFitWidth = 0
     st.view.numberOfLines = 0
-
-    st.frame = {bp: 10, w: 300, h: 30}
+    st.frame = {bp: 10, w: st.super_width, h: 30}
     st.text = "Impact Type: Allows unauthorized disclosure of information; Allows unauthorized modification; Allows disruption of service"
-    st.color = color.red
+    st.color = color.black
     st.font = font.small
   end
-
-
 end
