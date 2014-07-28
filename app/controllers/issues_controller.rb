303c300626a700769c3be92a59451c3972e11171
gregmcguirk <greg@paynearme.com>
2014-07-27 21:56:47 -0700

    style update

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -2,7 +2,6 @@ class IssuesController < UITableViewController
 
   ISSUES_CELL_ID = "IssuesCell"
 
-    # @data = [{:name=>"CVE-2014-4937", :num=>55}, {:name=>"CVE-2014-4938", :num=>21}, {:name=>"CVE-2014-4939", :num=>17}, {:name=>"CVE-2014-4940", :num=>73}, {:name=>"CVE-2014-4941", :num=>41}, {:name=>"CVE-2014-4942", :num=>37}]
   def initWithData(data)
     self.initWithNibName(nil, bundle: nil)
     @data = data
@@ -33,7 +32,6 @@ class IssuesController < UITableViewController
   def tableView(table_view, cellForRowAtIndexPath: index_path)
     data_row = @data[index_path.row]
     cell = table_view.dequeueReusableCellWithIdentifier(ISSUES_CELL_ID) || begin
-      puts "#{ISSUES_CELL_ID}"
       rmq.create(IssuesCell, :issues_cell, reuse_identifier: ISSUES_CELL_ID).get
 
       # If you want to change the style of the cell, you can do something like this:
@@ -46,7 +44,6 @@ class IssuesController < UITableViewController
   end
 
   def open_show_issue_controller
-    puts "\t\t\t#{Thread.current}"
     controller = ShowIssueController.alloc.initWithNibName(nil, bundle: nil)
     self.navigationController.pushViewController(controller, animated: true)
   end
--- app/controllers/show_issue_controller.rb
+++ app/controllers/show_issue_controller.rb
@@ -12,18 +12,31 @@ class ShowIssueController < UIViewController
   end
 
   def append_labels
-    labels = [ :release_date_label, :release_date_body,
-               :last_revised_label, :last_revised_body,
-               :source_label, :source_body,
-               :summary_label, :summary_body,
-               :cvss_severity_label,:cvss_severity_body,
-               :cvss_base_score_label,:cvss_base_score_body,
-               :impact_subscore_label,:impact_subscore_body,
-               :explotability_subscore_label,:explotability_subscore_body,
-               :access_vector_label,:access_vector_body,
-               :access_complexity_label,:access_complexity_body,
-               :authentication_label,:authentication_body,
-               :impact_type_label,:impact_type_body]
+    labels = [ :release_date_label,
+               :release_date_body,
+               :last_revised_label,
+               :last_revised_body,
+               :source_label,
+               :source_body,
+               :summary_label,
+               :summary_body,
+               :cvss_severity_label,
+               :cvss_severity_body,
+               :cvss_base_score_label,
+               :cvss_base_score_body,
+               :impact_subscore_label,
+               :impact_subscore_body,
+               :explotability_subscore_label,
+               :explotability_subscore_body,
+               :access_vector_label,
+               :access_vector_body,
+               :access_complexity_label,
+               :access_complexity_body,
+               :authentication_label,
+               :authentication_body,
+               :impact_type_label,
+               :impact_type_body ]
+
     labels.each {|label| rmq(@scroll_view).append(UILabel, label) }
   end
 end
